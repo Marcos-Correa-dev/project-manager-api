cf8b60e5ec93e8a59d4fb7e9f298d13cfba41128
marcos.correa <marcos.correa@fpf.br>
2025-10-31 10:42:05 -0400

    release(feat): user-cases

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UseCasesModule } from './use-cases/use-cases.module';
+import { GetAllProjectsService } from './user-cases/projects/get-all-projects.service';
+import { GetProjectsByIdService } from './user-cases/projects/get-projects-by-id.service';
+import { CreateProjectService } from './user-cases/projects/create-project.service';
 
 @Module({
   imports: [UseCasesModule],
+  providers: [GetAllProjectsService, GetProjectsByIdService, CreateProjectService],
 })
 export class DomainModule {}
