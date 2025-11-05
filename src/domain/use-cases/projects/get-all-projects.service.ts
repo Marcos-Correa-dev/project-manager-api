1e0fb1fff2d8149744f34685be332773ae3a0605
marcos.correa <marcos.correa@fpf.br>
2025-11-05 17:46:06 -0400

    feat(release): Implements Users Cases by projects(create, get by id, get-all-projects)

--- src/domain/use-cases/projects/get-all-projects.service.ts
+++ src/domain/use-cases/projects/get-all-projects.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { BaseUseCase } from '../base-use-case';
+import { ProjectsRepositoryService } from '../../../infrastructure/database/repositories/projects.repository.service';
+import { UsersRepositoryService } from '../../../infrastructure/database/repositories/users.repository.service';
+import { IProject } from '../../interfaces/project.interface';
 
 @Injectable()
-export class GetAllProjectsService {}
+export class GetAllProjectsService implements BaseUseCase {
+  constructor(
+    private readonly projectsRepository: ProjectsRepositoryService,
+    private readonly usersRepository: UsersRepositoryService,
+  ) {}
+
+  async execute(userId: number): Promise<IProject[]> {
+    const userData = await this.usersRepository.findById(userId);
+
+    const projects = await this.projectsRepository.findAll(userData.id);
+
+    if (!projects) {
+      throw new Error('Projects not found');
+    }
+
+    return projects;
+  }
+}
