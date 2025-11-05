6d7ac2fed7ced4a1463d88677a5816d71530278b
marcos.correa <marcos.correa@fpf.br>
2025-11-05 16:53:10 -0400

    Release/create methold user service and get user service

--- src/domain/use-cases/users/get-use-by-idr.service.ts
+++ src/domain/use-cases/users/get-use-by-idr.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { BaseUseCase } from '../base-use-case';
+import { UsersRepositoryService } from '../../../infrastructure/database/repositories/users.repository.service';
+import { IUser } from '../../interfaces/user.interface';
 
 @Injectable()
-export class GetUseByIdrService {}
+export class GetUseByIdrService implements BaseUseCase {
+  constructor(private readonly usersRepository: UsersRepositoryService) {}
+
+  async execute(userId: number): Promise<IUser> {
+    return this.usersRepository.findById(userId);
+  }
+}
